610def765fe37218d460a9649e1062c4e94dd5e2
Daniel Parks <daniel.parks@bbc.co.uk>
2016-05-20 13:22:32 +0100

    added shared items

---
+++ src/main/scala/challenger/sharedItems/SharedItems.scala
@@ -0,0 +1,15 @@
+package challenger.sharedItems
+
+object SharedItems {
+  def main(args: Array[String]) = {
+    val lOne: List[String] = List("first", "second", "third")
+    val lTwo: List[String] = List("fourth", "second", "first")
+
+    val duplicates = for {
+      one <- lOne
+      two <- lTwo if one == two
+    } yield one
+
+    println(s"following items are shared between lists: $duplicates")
+  }
+}
